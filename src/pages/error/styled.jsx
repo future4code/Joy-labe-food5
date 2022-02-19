8b078e86242f68d6c8f5765ad062e9422da7db20
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2022-02-19 00:51:17 -0300

    pagina erro styled

--- src/pages/error/styled.jsx
+++ src/pages/error/styled.jsx
@@ -2,10 +2,8 @@ import styled from "styled-components";
 
 export const ConteinerErro = styled.div`
 height:100vh;
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-background-size: cover;
+background-size:300%;
+
+background-position: 50%;
 background-image: url("https://studio.uxpincdn.com/studio/wp-content/uploads/2021/06/10-error-404-page-examples-for-UX-design.png")
 `
