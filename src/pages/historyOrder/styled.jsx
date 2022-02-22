4a5aafb5fd0a99d8717c7136a66adf63870ffbb6
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2022-02-21 23:26:32 -0300

    ajuste Pagina Historico

---
+++ src/pages/historyOrder/styled.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const ContainerHistorico = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 0.5px solid ;
+    border-radius: 15px;
+    width: 80vw;
+    margin: 10vw;
+
+    .span {
+    color: #E8222E;
+    display: flex;
+    width: 25vw;;
+    align-items: center;
+    justify-content:center;
+    justify-content:space-evenly;
+  }
+`
+
+export const ConteinerInformacao = styled.div`
+    padding: 12vw;
+ 
+`
